21a3578402e0c24861e7523b28cd6b4443a5e443
Todd Greener <todd.greener@gmail.com>
2018-06-18 01:28:24 -0600

    Initial Commit

---
+++ RPG Character/RPG_Character.h
@@ -0,0 +1,19 @@
+//
+//  RPG_Character.h
+//  RPG Character
+//
+//  Created by Todd Greener on 6/18/18.
+//  Copyright © 2018 Todd Greener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RPG_Character.
+FOUNDATION_EXPORT double RPG_CharacterVersionNumber;
+
+//! Project version string for RPG_Character.
+FOUNDATION_EXPORT const unsigned char RPG_CharacterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPG_Character/PublicHeader.h>
+
+
